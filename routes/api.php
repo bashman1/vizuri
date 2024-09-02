4a71d6780ab71bc2cfa3f488744a9b4d7a7cccc8
Wamula Bashir Saidi <wamulabash1@gmail.com>
2024-09-02 12:22:45 +0300

    worked on the authentication

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,11 @@ Route::get('/vizuri', function () {
 
 Route::post("create-user", [UserController::class, "createUser"]);
 Route::post("login", [UserController::class, "login"]);
-Route::post("create-company", [CompanyController::class, "createCompany"]);
-Route::post("create-sector", [CompanyController::class, "createSector"]);
-Route::get("get-sectors", [CompanyController::class, "getSectors"]);
-Route::get("get-companies", [CompanyController::class, "getCompaniesList"]);
-Route::get("get-company-details/{id}", [CompanyController::class, "getCompanyDetails"]);
+
+Route::group(['middleware'=>["auth:api"]], function(){
+    Route::post("create-company", [CompanyController::class, "createCompany"]);
+    Route::post("create-sector", [CompanyController::class, "createSector"]);
+    Route::get("get-sectors", [CompanyController::class, "getSectors"]);
+    Route::get("get-companies", [CompanyController::class, "getCompaniesList"]);
+    Route::get("get-company-details/{id}", [CompanyController::class, "getCompanyDetails"]);
+ });
